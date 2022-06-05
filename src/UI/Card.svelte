98731ca86e4ade9cd8966747833d3f1f43c70ddd
Jason van Schaik <j.vanschaik@tcrmbo.nl>
2022-06-05 17:35:11 +0200

    feedback cards

---
+++ src/UI/Card.svelte
@@ -0,0 +1,18 @@
+<div class="card">
+  <!-- Before a component can accept children, though, 
+    it needs to know where to put them. 
+    You do this with the <slot> element. -->
+  <slot></slot>
+</div>
+
+<style>
+    .card {
+        background-color: white;
+        color: #333;
+        border-radius: 15px;
+        padding: 40px 50px;
+        margin: 20px 0;
+        position: relative
+
+    }
+</style>
